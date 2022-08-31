250e117d01e28412d3e6fab438f8686e5849928f
Ahmed Ben jemaa <ahmed.benjemaa@technica-engineering.de>
2022-08-31 03:54:15 +0100

    deployement

---
+++ root/precache-manifest.58493810657563fa626f22904282774f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf5965607a9406a31e9767ee8dbd1e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8031a72bba871f5ff537",
+    "url": "/static/css/main.af197178.chunk.css"
+  },
+  {
+    "revision": "06119e33fab45d903436",
+    "url": "/static/js/2.f9d82fd4.chunk.js"
+  },
+  {
+    "revision": "8031a72bba871f5ff537",
+    "url": "/static/js/main.5ab27ba6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
